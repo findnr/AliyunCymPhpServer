ac4daf7b273eff32c835d08008d3af3d5eb671b2
cym504875043 <504875043@qq.com>
2021-04-13 23:26:57 +0800

    add aliyun id orc

---
+++ src/message/Send.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AliyunCymPhpServer\message;
+
+
+use AlibabaCloud\Client\AlibabaCloud;
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
+
+class Send{
+
+    public function __construct($data=[])
+    {
+        AlibabaCloud::accessKeyClient($data['accessKeyId'], $data['accessKeyId'])->regionId('cn-hangzhou')->asDefaultClient();
+    }
+    public function sendOne($data=[]){
+        $result = AlibabaCloud::rpc()
+
+                ->product('Dysmsapi')
+
+                // ->scheme('https') // https | http
+
+                ->version('2017-05-25')
+
+                ->action('SendSms')
+
+                ->method('POST')
+
+                ->host('dysmsapi.aliyuncs.com')
+
+                ->options([
+
+                    'query' => [
+
+                        'RegionId' =>'cn-hangzhou',
+
+                        'SignName' => $data['signName'],
+
+                        'PhoneNumbers'  =>  $data['phnone'],
+
+                        'TemplateCode'  =>  $data['template_code'],
+
+                        'TemplateParam' =>  json_encode($data['content']),
+
+                    ],
+
+                ])->request();
+    }
+}
